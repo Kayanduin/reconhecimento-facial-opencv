9fefc90ea7d6a64ed6e353e893c0c1c99f143e89
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-09-06 22:45:17 -0300

    feat: add photos of jonatas

--- reconhecedor_lbph.py
+++ reconhecedor_lbph.py
@@ -22,6 +22,8 @@ while (True):
             nome = 'Rafael'
         elif id == 2:
             nome = 'Gabriel'
+        elif id == 3:
+            nome = 'Jonatas'
         cv2.putText(imagem, nome, (x, y + (a + 30)), font, 2, (0, 0, 255))
         cv2.putText(imagem, str(confianca), (x, y + (a + 50)), font, 1, (0, 0, 255))
 
