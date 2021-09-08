4fa122e66d026888163ba7f8431c44d4c12caf60
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-09-08 09:20:05 -0300

    feat: thiago

--- reconhecedor_lbph.py
+++ reconhecedor_lbph.py
@@ -14,7 +14,8 @@ while (True):
                                                     scaleFactor=1.5,
                                                     minSize=(30, 30))
     for (x, y, l, a) in facesDetectadas:
-        imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+        imagemFace = cv2.resize(
+            imagemCinza[y:y + a, x:x + l], (largura, altura))
         cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 2)
         id, confianca = reconhecedor.predict(imagemFace)
         nome = ""
@@ -24,8 +25,11 @@ while (True):
             nome = 'Gabriel'
         elif id == 3:
             nome = 'Jonatas'
+        elif id == 5:
+            nome = 'Thiago'
         cv2.putText(imagem, nome, (x, y + (a + 30)), font, 2, (0, 0, 255))
-        cv2.putText(imagem, str(confianca), (x, y + (a + 50)), font, 1, (0, 0, 255))
+        cv2.putText(imagem, str(confianca),
+                    (x, y + (a + 50)), font, 1, (0, 0, 255))
 
     cv2.imshow("Face", imagem)
     if cv2.waitKey(1) == ord('q'):
